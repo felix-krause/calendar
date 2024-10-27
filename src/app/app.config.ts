16fca8e906a989deb978e91ae7ed8311a35881f0
felix-krause <felix.krause@icloud.com>
2024-10-27 16:10:08 +0100

    remove mat theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -30,6 +30,7 @@ export const appConfig: ApplicationConfig = {
           deps: [HttpClient],
         },
       }),
-    ), provideAnimationsAsync(),
+    ),
+    provideAnimationsAsync(),
   ],
 };
