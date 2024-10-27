7fa984ea0d7e97a73bccf54f4b03c5a691ee1a4d
felix-krause <felix.krause@icloud.com>
2024-10-27 15:44:56 +0100

    remove tailwind

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -30,6 +30,6 @@ export const appConfig: ApplicationConfig = {
           deps: [HttpClient],
         },
       }),
-    ),
+    ), provideAnimationsAsync(),
   ],
 };
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  darkMode: "media",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
